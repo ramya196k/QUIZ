da9d11e1725803c1a73c003549738763eee92fd2
ramya196k <kotaramya91@gmail.com>
2025-10-24 15:48:19 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "mongodb+srv://mahidhanush20_db_user:WgDK1D1hAFsWt7el@quiz.ns8ufje.mongodb.net/?appName=QUIZ" || process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
