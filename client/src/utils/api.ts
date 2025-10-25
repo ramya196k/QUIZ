71397e345651c01319162c116df0dd9df6bd2bb7
Hosanna-Mosa <Hosannamosa4190@gmail.com>
2025-10-25 09:02:41 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = "https://quiz-servi.onrender.com/api" || 'http://localhost:5000/api';
+const API_BASE_URL = "https://quiz-servi.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
