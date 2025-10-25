e42470865097244a2ba31d614dcf1a1f64c7025f
Hosanna-Mosa <Hosannamosa4190@gmail.com>
2025-10-25 09:23:52 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:  process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://quiz-front-ag6t.onrender.com" || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
