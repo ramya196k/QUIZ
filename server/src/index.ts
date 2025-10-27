4bfeb8d5401783f52b71f90e2a077eec2193f3b4
Dhanush-Kovilapu <123237548+Dhanush9866@users.noreply.github.com>
2025-10-27 19:31:10 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-  origin:  "http://localhost:5173",
+  origin:  "https://quiz-front-ag6t.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
